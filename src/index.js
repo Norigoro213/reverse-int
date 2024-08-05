7c86ac218459b9e98fe0cf14fc533d66b38ff84a
Norigoro213 <superkonfetka1408@gmail.com>
2024-08-05 10:28:31 +0300

    feat: complete the task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
 module.exports = function reverse (n) {
-  
+    const strN = Math.abs(n).toString();
+    const revStr = parseInt(strN.split('').reverse().join('').toString());
+    return revStr;       
 }
